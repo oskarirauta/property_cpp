d86a5b6ab72f8499e9ae98a2725fdbc2d6ed3820
Oskari Rauta <oskari.rauta@gmail.com>
2021-10-21 00:07:46 +0300

    propertycpp: make operators inline
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- include/property.hpp
+++ include/property.hpp
@@ -57,11 +57,11 @@ class Property {
 		Setter set;
 };
 
-std::ostream &operator<<(std::ostream &os, Property<std::string>::ReadOnly const &m) {
+inline std::ostream &operator<<(std::ostream &os, Property<std::string>::ReadOnly const &m) {
 	return os << std::string(m);
 }
 
-std::ostream &operator<<(std::ostream &os, Property<std::string> const &m) {
+inline std::ostream &operator<<(std::ostream &os, Property<std::string> const &m) {
 	return os << std::string(m);
 }
 
